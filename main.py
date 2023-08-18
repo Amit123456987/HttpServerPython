4d605946320b3008cc43eeeb29b3b5b91fcaaa31
Amit123456987 <43910117+Amit123456987@users.noreply.github.com>
2023-08-18 20:53:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
-import kivy
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-import requests
+# echo-server.py
+import socket
+import pyautogui
+import subprocess
 
 
-    
-# print(f"Status Code: {r.status_code}, Content: {r.json()}")
+def recognize(MyText):
+    if (MyText == "chrome"):
+        subprocess.Popen(
+            'C:\Program Files\Google\Chrome\Application\chrome.exe')
 
-class MyApp(App) :
-    # def __init__(self):
-    #     self.input_field = None
-    #     self.button = None
-    #     self.root = None
+    elif (MyText.find('tap') >= 0):
+        pyautogui.doubleClick()
+
+    elif (MyText.find('select line') >= 0):
+        pyautogui.keyDown('shiftleft')  # hold down the shift key
+        pyautogui.keyDown('shiftright')
+
+        pyautogui.press('end')
+
+        pyautogui.keyUp('shiftright')
+        pyautogui.keyUp('shiftleft')
+
+    elif (MyText.find('down') >= 0):
+        pyautogui.keyDown('shiftleft')  # hold down the shift key
+        pyautogui.keyDown('shiftright')
+
+        pyautogui.press('down', presses=5, interval=0.1)
+
+        pyautogui.keyUp('shiftleft')
+        pyautogui.keyUp('shiftright')
+
+    elif (MyText.find('copy') >= 0):
+        pyautogui.hotkey('ctrl', 'c')
+
+    elif (MyText.find('best') >= 0):
+        pyautogui.hotkey('ctrl', 'v')
+
+    elif (MyText.find('back') >= 0):
+        pyautogui.hotkey('ctrl', 'z')
+
+    elif (MyText.find('cut') >= 0):
+        pyautogui.hotkey('del')
 
 
-    def build(self):
-        self.root = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
+HOST = "192.168.1.6"  # Standard loopback interface address (localhost)
+PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
 
-        # Add an input field
-        self.input_field = TextInput(text="Enter your text here")
-        self.root.add_widget(self.input_field)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            data = data.decode('utf-8')
 
-        # Add a button
-        self.button = Button(text="Click me!")
-        self.button.bind(on_press=self.send)
+            splitdata = data.split('\r\n')
+            print(splitdata)
 
-        self.root.add_widget(self.button)
-
-        return self.root
-    
-    def send(self,button):
-        text = self.input_field.text
-        print(text)
-        requests.post('http://192.168.1.6:8000',headers={"text":text})
-
-
-MyApp().run()
+            if( len(splitdata[-1]) > 0 ):
+                extractedEquation = splitdata[-1].split('=')
+                commandText = extractedEquation[1].replace("+", " ")
+                recognize(commandText)
