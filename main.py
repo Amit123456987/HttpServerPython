faf366151337ffea63888f2dd6e1348370e5b4e1
Amit123456987 <43910117+Amit123456987@users.noreply.github.com>
2023-08-19 12:10:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ import subprocess
 
 
 def recognize(MyText):
+    print(MyText)
+    if( MyText[:5] == "right" ):
+        wordToWrite = MyText[6:].replace(' dot ','.') 
+        pyautogui.write(wordToWrite)
+
     if (MyText == "chrome"):
         subprocess.Popen(
             'C:\Program Files\Google\Chrome\Application\chrome.exe')
 
-    elif (MyText.find('tap') >= 0):
+    if (MyText.find('tap') >= 0):
         pyautogui.doubleClick()
 
-    elif (MyText.find('select line') >= 0):
+    if (MyText.find('select line') >= 0):
         pyautogui.keyDown('shiftleft')  # hold down the shift key
         pyautogui.keyDown('shiftright')
 
-        pyautogui.press('end')
+        pyautogui.press('down')
 
         pyautogui.keyUp('shiftright')
         pyautogui.keyUp('shiftleft')
 
-    elif (MyText.find('down') >= 0):
+    if (MyText.find('down') >= 0):
         pyautogui.keyDown('shiftleft')  # hold down the shift key
         pyautogui.keyDown('shiftright')
 
@@ -30,16 +35,28 @@ def recognize(MyText):
         pyautogui.keyUp('shiftleft')
         pyautogui.keyUp('shiftright')
 
-    elif (MyText.find('copy') >= 0):
+    if (MyText.find('copy') >= 0):
+        print("copy")
         pyautogui.hotkey('ctrl', 'c')
 
-    elif (MyText.find('best') >= 0):
+    if (MyText.find('best') >= 0):
+        
+        pyautogui.hotkey('ctrl', 'v')
+    
+    if (MyText.find('paste') >= 0):
+        pyautogui.keyDown('shiftleft')  # hold down the shift key
+        pyautogui.keyDown('shiftright')
+
         pyautogui.hotkey('ctrl', 'v')
 
-    elif (MyText.find('back') >= 0):
+        pyautogui.keyUp('shiftright')
+        pyautogui.keyUp('shiftleft')
+        
+
+    if (MyText.find('back') >= 0):
         pyautogui.hotkey('ctrl', 'z')
 
-    elif (MyText.find('cut') >= 0):
+    if (MyText.find('cut') >= 0):
         pyautogui.hotkey('del')
 
 
@@ -49,18 +66,20 @@ PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            data = data.decode('utf-8')
+    
+    # with conn:
+    #     print(f"Connected by {addr}")
+    while True:
+        conn, addr = s.accept()
+        
+        data = conn.recv(1024)
+        print(data)
+        data = data.decode('utf-8')
 
-            splitdata = data.split('\r\n')
-            print(splitdata)
+        splitdata = data.split('\r\n')
+        print(splitdata)
 
-            if( len(splitdata[-1]) > 0 ):
-                extractedEquation = splitdata[-1].split('=')
-                commandText = extractedEquation[1].replace("+", " ")
-                recognize(commandText)
+        if( len(splitdata[-1]) > 0 ):
+            extractedEquation = splitdata[-1].split('=')
+            commandText = extractedEquation[1].replace("+", " ")
+            recognize(commandText)
